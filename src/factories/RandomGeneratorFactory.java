0986d7654d21a4a071a98ccaf2e090a020559bc8
Szymon Wojciechowski <sw10@interia.pl>
2015-05-27 22:52:02 +0200

    Dodany nowy rodzaj generatora losowego: HighQualityRandom

--- src/factories/RandomGeneratorFactory.java
+++ src/factories/RandomGeneratorFactory.java
@@ -1,16 +1,27 @@
 package factories;
 
 import generator.GaussianDistributionGenerator;
+import generator.HighQualityRandom;
 import generator.RandomGenerator;
 import generator.UniformDistributionGenerator;
 
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
 import java.util.Random;
 
 
 public class RandomGeneratorFactory
 {
-	public final static long RANDOM_SEED = System.nanoTime();
-	private static final Random random = new Random(RANDOM_SEED);
+	public final static long RANDOM_SEED = generateRandomSeed();
+	private static final Random random = new HighQualityRandom(RANDOM_SEED);//new SecureRandom();//new Random(RANDOM_SEED);
+	
+	private static long generateRandomSeed()
+	{
+	    ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+	    buffer.put(SecureRandom.getSeed(Long.BYTES));
+	    buffer.flip();
+	    return buffer.getLong();
+	}
 	
 	public static RandomGenerator makeCoreExamplesGenerator(RegionDescription c)
 	{
